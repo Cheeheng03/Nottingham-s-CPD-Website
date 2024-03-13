4f6b36e210964b2457394e73eef0f615087924de
Cheeheng03 <cheehengtang03@gmail.com>
2024-03-13 17:59:09 +0800

    added loading to ensure metamask server connected before user clicking connect button

--- src/pages/connect.js
+++ src/pages/connect.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ethers } from 'ethers';
 import { StudentInfoAddress, StudentInfoAbi } from '../Address&Abi/StudentRegistryContract';
@@ -7,10 +7,24 @@ function Connect() {
     const [isMetaMaskConnected, setIsMetaMaskConnected] = useState(false);
     const navigate = useNavigate();
 
+    useEffect(() => {
+        initializeMetaMask();
+    }, []);
+
+    const initializeMetaMask = async () => {
+        if (window.ethereum) {
+            try {
+                await window.ethereum.request({ method: 'eth_requestAccounts' });
+                setIsMetaMaskConnected(true);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    };
+
     const fetchMetaMaskAddress = async () => {
         try {
             const provider = new ethers.providers.Web3Provider(window.ethereum);
-            await provider.send('eth_requestAccounts', []);
             const signer = provider.getSigner();
             const address = await signer.getAddress();
             return address;
@@ -24,13 +38,13 @@ function Connect() {
         try {
             const accountAddress = await fetchMetaMaskAddress();
             if (accountAddress) {
-                setIsMetaMaskConnected(true);
                 const provider = new ethers.providers.Web3Provider(window.ethereum);
                 const signer = provider.getSigner();
                 const studentContract = new ethers.Contract(StudentInfoAddress, StudentInfoAbi, signer);
                 const studentInfo = await studentContract.getStudentInfoByAddress(accountAddress);
                 console.log(studentInfo);
-                if (studentInfo &&
+                if (
+                    studentInfo &&
                     typeof studentInfo[0] !== "undefined" &&
                     typeof studentInfo[1] !== "undefined" &&
                     typeof studentInfo[2] !== "undefined" &&
@@ -40,11 +54,12 @@ function Connect() {
                     studentInfo[1] !== "" &&
                     studentInfo[2] !== "" &&
                     studentInfo[3] !== "" &&
-                    studentInfo[4] !== "0x0000000000000000000000000000000000000000") {
+                    studentInfo[4] !== "0x0000000000000000000000000000000000000000"
+                ) {
                     navigate('/home');
                 } else {
-                    alert('You are not yet registered.');
-                }                            
+                    alert('You are not yet registered. Please Sign Up for an account!');
+                }
             } else {
                 alert('No MetaMask account selected.');
             }
@@ -52,7 +67,7 @@ function Connect() {
             console.error(error);
             alert('Failed to connect to MetaMask. Please make sure MetaMask is installed and unlocked.');
         }
-    };    
+    };
 
     const signUp = () => {
         navigate('/signup');
@@ -62,11 +77,19 @@ function Connect() {
         <div className="bg-gray-50 min-h-screen flex items-center justify-center">
             <div className="bg-gray-100 rounded-2xl shadow-lg max-w-md mx-auto p-8">
                 <h2 className="font-bold text-2xl text-[#002D74] mb-6 text-center">Connect to MetaMask</h2>
-                <button 
-                    className="w-full bg-[#002D74] text-white font-bold py-2 px-4 rounded hover:bg-[#002D74]/90 focus:outline-none focus:shadow-outline mb-4" 
-                    onClick={connectToMetaMask}>
-                    Connect
-                </button>
+                {isMetaMaskConnected ? (
+                    <button 
+                        className="w-full bg-[#002D74] text-white font-bold py-2 px-4 rounded hover:bg-[#002D74]/90 focus:outline-none focus:shadow-outline mb-4" 
+                        onClick={connectToMetaMask}>
+                        Connect
+                    </button>
+                ) : (
+                    <button 
+                        className="w-full bg-gray-300 text-white font-bold py-2 px-4 rounded hover:bg-[#002D74]/90 focus:outline-none focus:shadow-outline mb-4" 
+                        disabled={true}>
+                        Loading
+                    </button>
+                )}
                 <button 
                     className="w-full bg-gray-300 text-[#002D74] font-bold py-2 px-4 rounded hover:bg-gray-400 focus:outline-none focus:shadow-outline" 
                     onClick={signUp}>
