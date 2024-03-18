ee492cd30479fdf6da500d5577a2b4292c007e5f
Jeremoot <84500442+Jeremoot@users.noreply.github.com>
2024-03-19 02:40:38 +0800

    Update frontend

--- src/pages/studentevent.js
+++ src/pages/studentevent.js
@@ -2,18 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
-import Collapsible from 'react-collapsible';
 import { eventRegistryContractAddress, eventRegistryContractABI } from '../Address&Abi/EventRegistryContract'
 import { votingContractAddress, votingContractABI } from '../Address&Abi/VotingContract'
 
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 
 const StudentEvents = () => {
-    const [createdEvents, setCreatedEvents] = useState([]);
     const [openEvents, setOpenEvents] = useState([]);
     const [enrolledEvents, setEnrolledEvents] = useState([]);
-	const [pastEvents, setPastEvents] = useState([]);
-	const [signerAddress, setSignerAddress] = useState('');
+    const [pastEvents, setPastEvents] = useState([]);
+    const [activeTab, setActiveTab] = useState('open');
+    const [signerAddress, setSignerAddress] = useState('');
 
     useEffect(() => {
         async function fetchData() {
@@ -76,88 +75,46 @@ const StudentEvents = () => {
         fetchSignerAddress();
     }, []);
 
+    const renderEventCards = (events) => {
+        return events.map((event, index) => (
+            <div key={index} className="bg-white rounded-lg shadow-lg overflow-hidden flex flex-col">
+                <img src={`${event.ipfsHash}`} alt={event.name} className="w-full h-64 sm:h-72 object-cover" />
+                <div className="flex-1 p-4 flex flex-col justify-between">
+                    <div>
+                        <p className="text-lg font-semibold text-gray-800">Event ID: {event.eventId.toString()}</p>
+                        <p className="mt-1 text-gray-600">Name: {event.name}</p>
+                        <p className="mt-1 text-gray-600">Time: {new Date(event.time * 1000).toLocaleString()}</p>
+                        <p className="mt-1 text-gray-600">Venue: {event.venue}</p>
+                        <p className="mt-1 text-gray-600">Description: {event.description}</p>
+                        <p className="mt-1 text-gray-600">Tokens Rewarded: {event.finalTokens}</p>
+                    </div>
+                    <Link
+                        to={`/enroll/${event.eventId}`}
+                        className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 mt-4 text-center"
+                    >
+                        View
+                    </Link>
+                </div>
+            </div>
+        ));
+    };
+
     return (
         <div>
             <Navbar signerAddress={signerAddress} />
-            <h3 className="text-2xl font-bold text-center text-gray-800 mb-8">Student Events List</h3>
+            <div className="bg-gray-100 py-4 shadow">
+                <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <h3 className="text-2xl font-bold text-center text-gray-800 mb-4">Student Events List</h3>
+                    <div className="flex justify-center space-x-4 border-b border-gray-300">
+                        <div className="flex space-x-2">
+                            <button onClick={() => setActiveTab('open')} className={`px-4 py-2 uppercase font-semibold ${activeTab === 'open' ? 'text-blue-600 border-b-2 border-blue-600' : 'hover:text-blue-500'}`}>Open for Enrollment</button>
+                            <button onClick={() => setActiveTab('enrolled')} className={`px-4 py-2 uppercase font-semibold ${activeTab === 'enrolled' ? 'text-blue-600 border-b-2 border-blue-600' : 'hover:text-blue-500'}`}>Enrolled</button>
+                            <button onClick={() => setActiveTab('past')} className={`px-4 py-2 uppercase font-semibold ${activeTab === 'past' ? 'text-blue-600 border-b-2 border-blue-600' : 'hover:text-blue-500'}`}>Past</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-				<div className="collapsible-container">
-                    <Collapsible trigger={<div className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 mb-4">Events Open for Enrollment</div>} transitionTime={200}>
-						<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-4">
-                            {openEvents.map((event, index) => (
-                                <div key={index} className="bg-white rounded-lg shadow-lg overflow-hidden flex flex-col">
-                                    <img src={`${event.ipfsHash}`} alt={event.name} className="w-full h-64 sm:h-72 object-cover" />
-                                    <div className="flex-1 p-4 flex flex-col justify-between">
-                                        <div>
-                                            <p className="text-lg font-semibold text-gray-800">Event ID: {event.eventId.toString()}</p>
-                                            <p className="mt-1 text-gray-600">Name: {event.name}</p>
-                                            <p className="mt-1 text-gray-600">Time: {new Date(event.time * 1000).toLocaleString()}</p>
-                                            <p className="mt-1 text-gray-600">Venue: {event.venue}</p>
-                                            <p className="mt-1 text-gray-600">Description: {event.description}</p>
-                                            <p className="mt-1 text-gray-600">Tokens Rewarded: {event.finalTokens === 0 ? 5 : event.finalTokens}</p>
-                                        </div>
-                                        <Link
-                                            to={`/enroll/${event.eventId}`}
-                                            className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 mt-4 text-center"
-                                        >
-                                            View
-                                        </Link>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </Collapsible>
-                </div>
-
-                <div className="collapsible-container mt-4">
-                    <Collapsible trigger={<div className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 mb-4">Enrolled Events</div>} transitionTime={200}>
-						<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-4">
-                            {enrolledEvents.map((event, index) => (
-                                <div key={index} className="bg-white rounded-lg shadow-lg overflow-hidden flex flex-col">
-                                    <img src={`${event.ipfsHash}`} alt={event.name} className="w-full h-64 sm:h-72 object-cover" />
-                                    <div className="flex-1 p-4 flex flex-col justify-between">
-                                        <div>
-                                            <p className="text-lg font-semibold text-gray-800">Event ID: {event.eventId.toString()}</p>
-                                            <p className="mt-1 text-gray-600">Name: {event.name}</p>
-                                            <p className="mt-1 text-gray-600">Time: {new Date(event.time * 1000).toLocaleString()}</p>
-                                            <p className="mt-1 text-gray-600">Venue: {event.venue}</p>
-                                            <p className="mt-1 text-gray-600">Description: {event.description}</p>
-                                            <p className="mt-1 text-gray-600">Tokens Rewarded: {event.finalTokens === 0 ? 5 : event.finalTokens}</p>
-                                        </div>
-                                        <Link
-                                            to={`/enroll/${event.eventId}`}
-                                            className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 mt-4 text-center"
-                                        >
-                                            View
-                                        </Link>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </Collapsible>
-                </div>
-
-				<div className="collapsible-container mt-4">
-                    <Collapsible trigger={<div className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 mb-4">Past Events</div>} transitionTime={200}>
-						<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-4">
-                            {pastEvents.map((event, index) => (
-                                <div key={index} className="bg-white rounded-lg shadow-lg overflow-hidden flex flex-col">
-                                    <img src={`${event.ipfsHash}`} alt={event.name} className="w-full h-64 sm:h-72 object-cover" />
-                                    <div className="flex-1 p-4 flex flex-col justify-between">
-                                        <div>
-                                            <p className="text-lg font-semibold text-gray-800">Event ID: {event.eventId.toString()}</p>
-                                            <p className="mt-1 text-gray-600">Name: {event.name}</p>
-                                            <p className="mt-1 text-gray-600">Time: {new Date(event.time * 1000).toLocaleString()}</p>
-                                            <p className="mt-1 text-gray-600">Venue: {event.venue}</p>
-                                            <p className="mt-1 text-gray-600">Description: {event.description}</p>
-                                            <p className="mt-1 text-gray-600">Tokens Rewarded: {event.finalTokens === 0 ? 5 : event.finalTokens}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </Collapsible>
-                </div>
             </div>
         </div>
     );
